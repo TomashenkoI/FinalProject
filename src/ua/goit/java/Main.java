199846c04d5160ad8e7a7a97450bac351c788407
Grigogiy <g.gesiod@gmail.com>
2016-04-15 18:05:15 +0300

    Add class Input

--- src/ua/goit/java/Main.java
+++ src/ua/goit/java/Main.java
@@ -8,23 +8,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number:");
         String enteredNumber = scanner.nextLine();
-        int number = Integer.parseInt(enteredNumber);
 
-        if (number <= 0) {
-            System.out.println("Number should be more than 0");
-        }
-        if (number > 0 && number < 2) {
-            System.out.println("There is no prime numbers below " + number);
-        }
-        if (number > 2) {
-        try {
-            System.out.println("Prime numbers below " + number + ":");
-            System.out.println(SieveOfEratosthenes.numbersFiltering(number));
-        }
-        catch (NumberFormatException e) {
-            System.out.println("It must be a digital number!");
-        }
+        Input input = new Input();
+
+        int number = input.checkNumber(Integer.parseInt(enteredNumber));
+
+        System.out.println(SieveOfEratosthenes.numbersFiltering(number));
+
         }
     }
-}
+
 
